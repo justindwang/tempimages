35e551649da6520c0d4da3a3131f7fe4a635f0df
justindwang <justindwang@yahoo.com>
2021-12-09 17:10:00 -0800

    temp2

--- README.md
+++
@@ -1,1 +0,0 @@
-# CS170-Project2
--- accuracy.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <vector>
-
-using namespace std; 
-
-double crossValidation(vector< vector<double> > &data);
-void print(std::vector <double> const &a);
-void modified(vector<vector<double> > &data, vector<int> add_features, vector<vector<double> > &mod);
